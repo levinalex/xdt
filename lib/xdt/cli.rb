59417d129c403a0c17e813cc07f05848200cf6c5
Levin Alexander <mail@levinalex.net>
2012-07-05 10:26:02 +0200

    persist uuid between program runs

--- lib/xdt/cli.rb
+++ lib/xdt/cli.rb
@@ -1,8 +1,10 @@
 require 'gli'
 require 'json'
 require 'thread'
+require 'socket'
 require 'directory_watcher'
 require 'rest-client'
+require 'securerandom'
 
 
 module Xdt
@@ -20,8 +22,12 @@ module Xdt
     end
 
     def watch!(&block)
-      while fname = @queue.pop do
-        process_file(fname, &block)
+      begin
+        while fname = @queue.pop do
+          process_file(fname, &block)
+        end
+      rescue Interrupt => e
+        puts "Quitting (Interrupt)... "
       end
     end
 
@@ -34,23 +40,46 @@ module Xdt
     end
   end
 
-
   module CLI
     extend GLI::App
 
     program_desc "XDT parser"
     version Xdt::VERSION
 
+    flag [:config,:c], default_value: File.join(ENV['HOME'],'.xdt'),
+                       desc: "stores configuration and authentication data",
+                       arg_name: "FILE"
+
     desc "watch a directory for GDT files and post extracted patient information to the given HTTP endpoint"
     arg_name "directory"
+
+    around do |global_options,command,options,args,code|
+      fname = global_options[:config]
+      global_options[:config] = if fname && File.exist?(fname)
+                                  YAML.load_file(fname)
+                                else
+                                  {}
+                                end
+      global_options[:config]["uuid"] ||= SecureRandom.uuid
+
+      code.call
+
+      File.open(fname, "w+") { |f| f.write(global_options[:config].to_yaml) }
+    end
+
+
     command :watch do |c|
-      c.flag "uri", type: String, desc: "HTTP endpoint where patients are posted to", arg_name: "URI"
+      c.flag [:uri,:e], type: String,
+                        desc: "HTTP endpoint where patients are posted to",
+                        arg_name: "URI"
 
       c.action do |global_options,options,args|
         help_now!('directory is required') if args.empty?
         Xdt::Watcher.new(args).watch! do |patient_data|
           puts patient_data.to_json
-          RestClient.post options[:uri], {:patient => patient_data, :ua => Xdt.version}, :content_type => :json, :accept => :json
+          opts = { uuid: global_options[:config]["uuid"], hostname: Socket.gethostname, program: Xdt.version }
+
+          RestClient.post options[:uri], {:patient => patient_data, :about => opts }, :content_type => :json, :accept => :json
         end
       end
     end
